af3190b9ce80a7f2d720636370e944bcd9f5c087
Jackeagle <jackeagle102@gmail.com>
2015-12-17 09:12:32 -0500

    a7lte: Add TimeService App
    Change-Id: I8069f9b0225a75184982b5af2cc8d8e0f147db22
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -17,18 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),samsung)
-ifneq ($(filter a5ultexx,$(TARGET_DEVICE)),)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
+ifneq ($(filter a7lte,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
